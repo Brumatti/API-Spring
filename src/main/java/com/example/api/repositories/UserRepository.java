4dcc40175e3a7d134aad5e70b084a5ca676d854b
Brumatti <pedrobrumatti@hotmail.com>
2022-08-07 18:30:21 -0300

    Interface user repository

--- src/main/java/com/example/api/repositories/UserRepository.java
+++ src/main/java/com/example/api/repositories/UserRepository.java
@@ -1,11 +1,9 @@
 package com.example.api.repositories;
 
-import com.example.api.domain.User;
+import com.example.api.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-
+public interface UserRepository extends JpaRepository<Users, Integer> {
 }
