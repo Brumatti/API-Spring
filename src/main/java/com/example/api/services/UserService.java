c0117e640fec1369b20bba22ee0029b0510a6a09
Brumatti <pedrobrumatti@hotmail.com>
2022-08-07 19:59:01 -0300

    Endpoint findAll para users

--- src/main/java/com/example/api/services/UserService.java
+++ src/main/java/com/example/api/services/UserService.java
@@ -2,6 +2,9 @@ package com.example.api.services;
 
 import com.example.api.domain.Users;
 
+import java.util.List;
+
 public interface UserService {
     Users findById(Integer id);
+    List<Users> findAll();
 }
