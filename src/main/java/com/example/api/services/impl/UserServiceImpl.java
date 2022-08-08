e188b0d05bed73c96ea407c60da3f234d1d8f413
Brumatti <pedrobrumatti@hotmail.com>
2022-08-08 01:06:06 -0300

    Endpoint Create para users

--- src/main/java/com/example/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.api.services.impl;
 
 import com.example.api.domain.Users;
+import com.example.api.domain.dto.UserDTO;
 import com.example.api.repositories.UserRepository;
 import com.example.api.services.UserService;
 import com.example.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,10 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public Users findById(Integer id) {
         Optional<Users> obj = repository.findById(id);
@@ -24,5 +30,10 @@ public class UserServiceImpl implements UserService {
 
     public List<Users> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public Users create(UserDTO obj) {
+        return repository.save(mapper.map(obj, Users.class));
     }
 }
